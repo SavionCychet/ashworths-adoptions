0ab56915658f1523c7de669fe30d20ee74d13921
SavionCychet <enochcychet@gmail.com>
2020-01-30 09:41:35 -0600

    Created carousel

--- ashworths-adoptions/src/app/app.module.ts
+++ ashworths-adoptions/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ImgsliderComponent } from './imgslider/imgslider.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ImgsliderComponent
   ],
   imports: [
     BrowserModule
---
+++ ashworths-adoptions/src/app/imgslider/imgslider.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'imgslider',
+  templateUrl: './imgslider.component.html',
+  styleUrls: ['./imgslider.component.css']
+})
+export class ImgsliderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
